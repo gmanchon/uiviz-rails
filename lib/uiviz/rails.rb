f307ec394f17f42c0a0eeec7ef405eca29be6d12
Gaetan Manchon <gmanchon@gmail.com>
2019-12-28 08:44:39 +0100

    initial commit

---
+++ lib/uiviz/rails.rb
@@ -0,0 +1,8 @@
+require "uiviz/rails/version"
+
+module Uiviz
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/uiviz/rails/version.rb
@@ -0,0 +1,5 @@
+module Uiviz
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
